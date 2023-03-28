7c8e06593f8d96b4f8f7f21cab77b41dfa024b91
johntrigg <jtrigg@usf.edu>
2023-03-28 16:52:17 -0400

    Added zsteg, binwalk, and strings

--- readme.md
+++ readme.md
@@ -3,3 +3,16 @@ Hello! This is a tool designed to help with steganography tools in CTFs. There a
 
 # How does it work?
 You feed it a file, and the filetype, and it'll run relevant tools on it. For example, you cannot run ztseg on a jpg file.
+
+# What tools does it run?
+On everything? Strings, exiftool, binwalk
+
+On PNG files? Zsteg
+
+On JPG? Stegseek
+
+# What tools do you still need to add?
+More research needed.
+
+# What still needs to be done?
+Better readme, as well as an installation guide for all of the tools, since it cannot reasonably be expected that the user has all of these tools done.
--- stegshotgun.py
+++ stegshotgun.py
@@ -27,9 +27,38 @@ def main():
     print("File type: " + fileType)
     universalTools(filename)
 
+    if fileType == "png":
+        pngTools(filename)
+    else:
+
+        jpgTools(filename)
+        
+
 
 def universalTools(filename):
-    print("[*]Running Binwalk")
+    print("[*] Running Binwalk")
     binwalkCommand = "binwalk -e " + filename
-    system.os(filename)
+    os.system(binwalkCommand)
+
+    print("[*] Running exiftool, putting output into exifdata.txt")
+    exiftoolCommand = "exiftool " + filename + " > exifdata.txt"
+    os.system(exiftoolCommand)
+
+    print("[*] Running strings, putting output into ImageStrings.txt")
+    stringsCommand = "strings " + filename + " > ImageStrings.txt"
+    os.system(stringsCommand)
+
+
+
+def pngTools(filename):
+    print("PNG detected, running PNG tools")
+    print("[*] Running zsteg")
+    zstegCommand = "zsteg -v -a " + filename
+
+def jpgTools(filename):
+    print("JPG detected, running JPG tools")
+    print("[*] Running stegseek")
+    stegseekCommand = "stegseek " + filename + " /usr/share/wordlists/rockyou.txt"
+    os.system(stegseekCommand)
+
 main()
